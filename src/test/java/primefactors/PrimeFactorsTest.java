a45ba754e8e0475956689b7f2a36a03ce9532c7e
Jake Lee <shibas11@gmail.com>
2017-07-07 01:09:28 +0900

    red - factorsOf(1)

--- src/test/java/primefactors/PrimeFactorsTest.java
+++ src/test/java/primefactors/PrimeFactorsTest.java
@@ -2,8 +2,24 @@ package primefactors;
 
 import org.junit.Test;
 
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
 public class PrimeFactorsTest {
-    @Test public void
-    nothing() {
+    @Test
+    public void
+    primeFactorsTest() {
+        assertThat(factorsOf(1), is(intList()));
+    }
+
+    private List<Integer> factorsOf(int n) {
+        return null;
+    }
+
+    private List<Integer> intList(int... args) {
+        return Arrays.asList();
     }
 }
