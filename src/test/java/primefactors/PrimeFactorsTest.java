04f508d2b11058ea4e92dcfca2c80276d087b7d6
Jake Lee <shibas11@gmail.com>
2017-07-07 01:20:56 +0900

    blue - factorsOf(9)

--- src/test/java/primefactors/PrimeFactorsTest.java
+++ src/test/java/primefactors/PrimeFactorsTest.java
@@ -22,22 +22,20 @@ public class PrimeFactorsTest {
         assertThat(factorsOf(7), is(intList(7)));
         assertThat(factorsOf(8), is(intList(2, 2, 2)));
         assertThat(factorsOf(9), is(intList(3, 3)));
+        assertThat(factorsOf(10), is(intList(2, 5)));
+        assertThat(factorsOf(12), is(intList(2, 2, 3)));
+        assertThat(factorsOf(14), is(intList(2, 7)));
+        assertThat(factorsOf(15), is(intList(3, 5)));
+        assertThat(factorsOf(2 * 2 * 3 * 5 * 7 * 13 * 17 * 17), is(intList(2, 2, 3, 5, 7, 13, 17, 17)));
     }
 
     private List<Integer> factorsOf(int n) {
         ArrayList<Integer> factors = new ArrayList<Integer>();
-        if (n > 1) {
-            while (n % 2 == 0) {
-                factors.add(2);
-                n /= 2;
-            }
-            while (n % 3 == 0) {
-                factors.add(3);
-                n /= 3;
-            }
-        }
-        if (n > 1)
-            factors.add(n);
+
+        for (int divisor = 2; n > 1; divisor++)
+            for (; n % divisor == 0; n /= divisor)
+                factors.add(divisor);
+
         return factors;
     }
 
