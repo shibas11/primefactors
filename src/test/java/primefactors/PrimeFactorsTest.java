9cc7ae0b3c9c7d4b1c7f797274a08b8d59f1dd95
Jake Lee <shibas11@gmail.com>
2017-07-07 01:15:46 +0900

    green - factorsOf(4)

--- src/test/java/primefactors/PrimeFactorsTest.java
+++ src/test/java/primefactors/PrimeFactorsTest.java
@@ -21,6 +21,12 @@ public class PrimeFactorsTest {
 
     private List<Integer> factorsOf(int n) {
         ArrayList<Integer> factors = new ArrayList<Integer>();
+        if (n > 1) {
+            if (n % 2 == 0) {
+                factors.add(2);
+                n /= 2;
+            }
+        }
         if (n > 1)
             factors.add(n);
         return factors;
